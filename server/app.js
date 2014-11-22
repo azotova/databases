3dbcde1e8b892eb10440fab0628b049f711353b8
azotova <azotova@gmail.com>
2014-11-21 20:57:35 -0800

    Serve the basic index page and have very basic tables

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var db = require('./db');
+var requ = require("./request-handler.js");
+var handleRequest = requ.requestHandler;
+var url = require('url');
+var path = require('path');
 
 // Middleware
 var morgan = require('morgan');
@@ -21,12 +25,31 @@ app.use(parser.json());
 // Set up our routes
 app.use("/classes", router);
 
+var whereTo = path.join(__dirname, "../client/client"); //path to static content
+
 // Serve the client files
-app.use(express.static(__dirname + "../client"));
+app.use(express.static(whereTo));
+
+/*app.get('/, function (req, res) {
+  var pathName = url.parse(req.url).pathname;
+  console.log("pathN", pathName);
+  res.send('Hello World!')
+});*/
 
 // If we are being run directly, run the server.
 if (!module.parent) {
   app.listen(app.get("port"));
   console.log("Listening on", app.get("port"));
 }
+
+/*GET /?username=Anastasia 304 4.977 ms - -
+GET /classes/messages?order=-createdAt - - ms - -
+GET /styles/styles.css 304 4.449 ms - -
+GET /bower_components/jquery/jquery.min.js 304 4.056 ms - -
+GET /bower_components/underscore/underscore-min.js 304 3.336 ms - -
+GET /env/config.js 304 3.366 ms - -
+GET /scripts/app.js 304 0.866 ms - -
+GET /images/spiffygif_46x46.gif 304 1.069 ms - -
+The requests from the file;
+*/
 
--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,34 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+createdAt varchar(50),
+objectId varchar(50),
+roomname varchar(50),
+messageText varchar (1000),
+updatedAt varchar (1000),
+username varchar(50),
+ID int(11) NOT NULL auto_increment, PRIMARY KEY (ID)
 );
+
+CREATE TABLE users (
+username varchar(50),
+ID int(11) NOT NULL auto_increment, PRIMARY KEY (ID)
+);
+
+CREATE TABLE rooms (
+roomname varchar(50),
+ID int(11) NOT NULL auto_increment, PRIMARY KEY (ID)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
+/* There is a problem. I want unique IDs of my rooms 
+and users in the messages table. Good link:
+http://www.w3schools.com/sql/sql_foreignkey.asp
 
+Should use drop to delete old tables.
+*/
 
 
 /*  Execute this file from the command line by typing:
